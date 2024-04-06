4f60b6cc03352eac20de7678d70d2c752c7b0b2f
San7o <santigio2003@gmail.com>
2024-04-06 19:03:43 +0200

    Added kill button and pretty lights

--- src/app-launcher.go
+++ src/app-launcher.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"github.com/pato/gonovation/launchpad"
     "os/exec"
+    "os"
 )
 
+// uname -a
 func runUname() {
     // Command to execute
     cmd := exec.Command("uname", "-a")
@@ -23,44 +25,78 @@ func runUname() {
 
 }
 
+// kitty
+func runKitty() {
+    // Command to execute
+    cmd := exec.Command("kitty")
+
+    // Execute the command and capture output
+    err := cmd.Start()
+    if err != nil {
+        fmt.Println("Error running the command \"kitty\":", err)
+        return
+    }
+
+    // Print the command output
+    fmt.Println("Command \"kitty\" has been completed without erros")
+
+}
+
+func resetLights(launchpad *gonovation.Launchpad) {
+     
+    launchpad.Reset()
+    
+    // Light avaiable buttons
+    r_orange := 3
+    g_orange := 3
+    launchpad.Led(0, 8, 3, 0)
+    launchpad.Led(0, 7, r_orange, g_orange)
+    launchpad.Led(1, 7, r_orange, g_orange)
+
+}
+
 func main() {
     
     // Setup
 	launchpad := gonovation.GetLaunchPad()
-    launchpad.Reset();
+    resetLights(launchpad)
 	fmt.Println("Initalized")
 	
     // Get events
     events := launchpad.Events()
-    
+   
     // Loop and check events
     for {
 		event := <-events
 		x, y, pressed := gonovation.EventInfo(event)
-        
+       
 		if pressed {
            
            // Light the pressed key
            fmt.Println("You pressed: %d, %d", x, y)
      	   launchpad.Led(int(x), int(y), 0, 3)
            
-        
            button := [2] int { int(x), int(y) }   
+           
            switch button {
            
            // Reset button
            case [2] int { 0, 8 }:
                 fmt.Println("Clearing Screen")
                 launchpad.Reset()
+                launchpad.Close()
+                os.Exit(0)
            case [2] int {0, 7}:
                 runUname()
+           case [2] int {1, 7}:
+                runKitty()
            default:
            }
 
         } else {
 
            // Clear all buttons that are not being pressed
-           launchpad.Reset()
+           resetLights(launchpad)
         }
 	}
 
